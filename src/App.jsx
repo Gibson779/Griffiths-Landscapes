402043eb05086d32af6fba327c870921422989dd
Gibson779 <79742592+Gibson779@users.noreply.github.com>
2026-03-09 13:54:27 +0000

    NewCommit

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,16 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>Hello world</h1>
-    
-      </div>
+
+    <Navbar>
+      <Route path = "/" elements = {<Home/>}/>
+      <Route path = "Home" elements = {<Home/>}/>
+      <Route path = "Services" elements = {<Services/>}/>
+      <Route path = "Gallery" elements = {<Gallery/>}/>
+      <Route path = "About" elements = {<About/>}/>
+      <Route path = "Reviews" elements = {<Reviews/>}/>
+      <Route path = "Contact" elements = {<Contact/>}/>
+    </Navbar>
      
     </>
   )
