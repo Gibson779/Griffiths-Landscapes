2eee591d3c14426580a81842cd9b489b11536134
Gibson779 <79742592+Gibson779@users.noreply.github.com>
2026-03-10 14:58:58 +0000

    NewCommit

--- src/route/Home.jsx
+++ src/route/Home.jsx
@@ -5,6 +5,15 @@ export default function Home(){
       <img src="../images/Home Image.jpg" alt="Home" className="home__image" />
        </div>
 
+       <div className="about__container">
+        <h1 className="about__header">ABOUT US</h1>
+        <p className="about__text">We take pride in providing homeowners across Greater Manchester with tailored paving and landscaping solutions that match their needs, style and budget. Our approach is built around careful planning, skilled execution, and durable materials. From initial consultation through to completion, our experienced team ensures a smooth, professional service. Every surface we lay and every space we shape is designed to enhance your property and stand up to the UK’s changing weather conditions.
+Whether it's a new driveway, a garden revamp, or seasonal maintenance, we approach every project with professionalism, care and a genuine desire to help you make the most of your outdoor space. </p>
+        <img src="./gallery/1.jpg" alt="About" className="about__image" />
+
+
+       </div>
+
         </>
     )
 }
